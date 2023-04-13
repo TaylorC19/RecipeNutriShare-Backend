2681b3abb8ad0030b69394ed6c2ae8ad3dad417b
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-04-13 15:24:06 +0900

    feat: recipecard component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Footer.css";
 
 function Footer(props) {
-  const { text, classname } = props;
+  const { text } = props;
   return (
     <div className='footer-div'>
       <p>{text}</p>
---
+++ client/src/components/RecipeCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./RecipeCard.css"
+
+function RecipeCard(props) {
+  const { title, servings, hours, minutes, calories, description } = props;
+  return (
+    <div className='recipe-card'>
+      <h2>{title}</h2>
+      <p>{servings}</p>
+      <p>CookTime {hours}:{minutes}</p>
+      <p>Calories per serving: {calories}</p>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+export default RecipeCard
