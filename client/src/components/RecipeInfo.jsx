4c2bc70ac87f7aa66381387d1cc6161ec64f39f0
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-04-17 15:56:51 +0900

    added keys for recipe info

--- client/src/components/RecipeInfo.jsx
+++ client/src/components/RecipeInfo.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
-function RecipeInfo() {
+function RecipeInfo(props) {
+  const { recipeInfo, setRecipeInfo } = props;
+
+  /*
+  {
+    title: "",
+    servings: "",
+    hours: "",
+    minutes: "",
+    description: "",
+    instructions: "",
+  }
+  */
+
+  const handleTitle = (e) => {};
+
   return (
-    <div>RecipeInfo</div>
-  )
+    <div>
+      <label htmlFor="title">Title</label>
+      <input
+        onChange={handleTitle}
+        type="text"
+        id="title"
+        placeholder="Title your recipe"
+      />
+    </div>
+  );
 }
 
-export default RecipeInfo
+export default RecipeInfo;
--- client/src/pages/NewRecipe.jsx
+++ client/src/pages/NewRecipe.jsx
@@ -1,37 +1,51 @@
-import React, { useState } from 'react';
-import Header from '../components/Header'
-import Ingredients from '../components/Ingredients';
-import RecipeInfo from '../components/RecipeInfo';
-import "./NewRecipe.css"
-
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Ingredients from "../components/Ingredients";
+import RecipeInfo from "../components/RecipeInfo";
+import "./NewRecipe.css";
 
 const NewRecipe = () => {
-  const [ingredientsArr, setIngredientsArr] = useState([{name:'', quantity: '', unit: ''}])
-  const [recipeInfo, setRecipeInfo] = useState({});
-  
+  const [ingredientsArr, setIngredientsArr] = useState([
+    { name: "", quantity: "", unit: "" },
+  ]);
+  const [recipeInfo, setRecipeInfo] = useState({
+    title: "",
+    servings: "",
+    hours: "",
+    minutes: "",
+    description: "",
+    instructions: "",
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const ingredientsArr2 = ingredientsArr.map((ingredient) => {
-      let string = `${ingredient.quantity} ${ingredient.unit} ${ingredient.name}`
+      let string = `${ingredient.quantity} ${ingredient.unit} ${ingredient.name}`;
       return string;
-    })
-    const ingredientsStr = ingredientsArr2.join(',');
-    console.log(ingredientsStr)
-  }
+    });
+    const ingredientsStr = ingredientsArr2.join(",");
+    console.log(ingredientsStr);
+  };
 
   return (
     <div>
       <Header />
       <h1>New Recipe</h1>
       <form onSubmit={handleSubmit}>
-        <div className='form-container'>
-        <Ingredients ingredientsArr={ingredientsArr} setIngredientsArr={setIngredientsArr}></Ingredients>
-        <RecipeInfo recipeInfo={recipeInfo} setRecipeInfo={setRecipeInfo}></RecipeInfo>
+        <div className="form-container">
+          <Ingredients
+            ingredientsArr={ingredientsArr}
+            setIngredientsArr={setIngredientsArr}
+          ></Ingredients>
+          <RecipeInfo
+            recipeInfo={recipeInfo}
+            setRecipeInfo={setRecipeInfo}
+          ></RecipeInfo>
         </div>
         <button type="submit">Submit new recipe</button>
       </form>
     </div>
   );
-}
+};
 
 export default NewRecipe;
