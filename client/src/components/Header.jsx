e985414be4b0073c94649e7180999470784cea1d
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-04-22 16:15:11 +0900

    attempt at navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,45 +1,49 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import './Header.css'
+import { useNavigate, Link } from "react-router-dom";
+import "./Header.css";
 import { UserAuth } from "./context/AuthContext";
 
 const Header = () => {
-  const {user, logOut} = UserAuth();
-  
+  const { user, logOut } = UserAuth();
+
   const navigate = useNavigate();
 
   const handleLogOut = async () => {
     try {
-      console.log('user', user);
-      await logOut(); 
-      navigate('/')
+      console.log("user", user);
+      await logOut();
+      navigate("/");
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
-  return(
-    <div className="header-div">
+  return (
+    <nav className="header-div">
       <h1 className="logo">Meal Prep Life</h1>
-        {Object.keys(user).length !== 0
-        ? 
-      <div className="navigate-items">
-        <button onClick={() => navigate('/')}>Home</button>
-        <button onClick={() => navigate('/public-recipes')}>Browse public recipes</button>
-        <button onClick={() => navigate('/new-recipe')}>New Recipe</button>
-        <button onClick={() => navigate('/my-recipes')}>My Recipes</button>
-        <button onClick={handleLogOut}>Log Out</button>
-      </div> 
-      :
-      <div className="navigate-items">
-        <button onClick={() => navigate('/')}>Home</button>
-        <button onClick={() => navigate('/public-recipes')}>Browse public recipes</button>
-        <button onClick={() => navigate('/signin')}>Sign In</button>
-        <button onClick={() => navigate('/signup')}>Sign Up</button>
-      </div>
-      }
-    </div>
-  )
-}
+      
+      {Object.keys(user).length !== 0 ? (
+        <div className="navigate-items">
+          <button onClick={() => navigate("/")}>Home</button>
+          <button onClick={() => navigate("/public-recipes")}>
+            Browse public recipes
+          </button>
+          <button onClick={() => navigate("/new-recipe")}>New Recipe</button>
+          <button onClick={() => navigate("/my-recipes")}>My Recipes</button>
+          <button onClick={handleLogOut}>Log Out</button>
+        </div>
+      ) : (
+        <div className="navigate-items">
+          <button onClick={() => navigate("/")}>Home</button>
+          <button onClick={() => navigate("/public-recipes")}>
+            Browse public recipes
+          </button>
+          <button onClick={() => navigate("/signin")}>Sign In</button>
+          <button onClick={() => navigate("/signup")}>Sign Up</button>
+        </div>
+      )}
+    </nav>
+  );
+};
 
 export default Header;
