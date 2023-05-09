f4dd876c27ca0a3948f78d7a23e7ec11c8eac6f3
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-05-09 15:22:26 +0900

    014 move firebase (#17)
    * feat: added firebase creds to react
    
    * feat: removed the users table.
    
    * feat: firebase is moved to frontend
    
    * feat: cleaned up consoles
    
    * feat: moved apikey
    
    * feat: moved config files

--- client/firebase/firebase.js
+++ client/firebase/firebase.js
@@ -1,14 +1,28 @@
 // Import the functions you need from the SDKs you need
 const { initializeApp } = require("firebase/app");
 const { getAuth } = require("firebase/auth");
-const firebaseConfig = require("./firebase_conf.js");
+//const firebaseConfig = require("./firebase_conf.js");
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAdIkOi7RzjIhd1Asq26YbD3-OWziqFy4I',
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID
+}
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optionals
+
+// console.log(firebaseConfig[process.env.REACT_APP_NODE_ENV].config)
+console.log(firebaseConfig)
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig[process.env.NODE_ENV].config);
+// const app = initializeApp(firebaseConfig[process.env.REACT_APP_NODE_ENV].config);
+const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
